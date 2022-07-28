a6208b5dd50cead564d8fe527dc102cc66573412
Unax <u@u.com>
2022-07-28 17:09:20 +0200

    Testing

--- tests/acceptance-flask/test_app_flask_tester.py
+++ tests/acceptance-flask/test_app_flask_tester.py
@@ -8,3 +8,18 @@ class TestApp(unittest.TestCase):
    
     def test_1_getToken(self):         
         self.assertTrue(True)
+
+        tester = app.test_client(self)
+
+        user_data = {"email":"jd@myinsuranceapp.com","password":"passwordjd"}
+
+        response = tester.post('/api/v1/token', content_type='application/json', json = user_data)
+
+        data=json.loads(response.text)
+
+        print(f"post token: {data}")
+
+        self.assertEqual(response.status_code, 200)
+
+        if response.status_code==200:
+            TestApp.token=data['token']

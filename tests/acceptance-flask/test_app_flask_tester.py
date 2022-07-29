0686c8d0895b9a3bedbdea646cb6c44b24b8abd0
Unax <u@u.com>
2022-07-29 09:33:13 +0200

    Corrections

--- tests/acceptance-flask/test_app_flask_tester.py
+++ tests/acceptance-flask/test_app_flask_tester.py
@@ -39,7 +39,7 @@ class TestApp(unittest.TestCase):
 
         self.assertTrue(len(data)>0)
 
-    """def test_3_get_user_products_invalid_token(self):
+    def test_3_get_user_products_invalid_token(self):
         tester = app.test_client(self)
 
         invalid_fake_token='CfDJ8OW5OI0CPGJBgSNlGwO0x4YF7qbYKVv7KOO-N0eFtDUzXOrL7F9Xd9W1otVi4ueJOkAmAhuoHFWNkqRaFD7zvAMHMSKncl6Vo5QXKmpvy6vqxOKxSURdIey8aZPRi3Nnhp2p9la-Al5xrVKz0lignRdcCHf3O7pF9zv_sNx_c_T7pUe3WsxaJEPX3t_9FO2Wjw'
@@ -52,7 +52,7 @@ class TestApp(unittest.TestCase):
         self.assertTrue(response.status_code > 400)
 
 
-    def test_4_invalid_user(self):
+    """def test_4_invalid_user(self):
         tester = app.test_client(self)
 
         print(f"token: {self.token}")

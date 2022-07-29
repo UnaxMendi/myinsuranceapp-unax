7fa0eb047cb1b5c7f52cc0502cec5775db87a83a
Unax <u@u.com>
2022-07-29 09:14:06 +0200

    Testing

--- tests/acceptance-flask/test_app_flask_tester.py
+++ tests/acceptance-flask/test_app_flask_tester.py
@@ -23,3 +23,55 @@ class TestApp(unittest.TestCase):
 
         if response.status_code==200:
             TestApp.token=data['token']
+            
+    def test_2_get_user_products_valid_token(self):
+        tester = app.test_client(self)
+
+        print(f"token: {self.token}")
+
+        headers = {"Autohrization": f"Bearer {TestApp.token}"}
+
+        response = tester.ge('/api/v1/users/1/1', content_type='application/json', headers=headers)
+
+        data=json.loads(response.text)
+        print(f"get_user_products: {data}")
+        self.assertEqual(response.status_code, 200)
+
+        self.assertTrue(len(data)>0)
+
+    def test_3_get_user_products_invalid_token(self):
+        tester = app.test_client(self)
+
+        invalid_fake_token='CfDJ8OW5OI0CPGJBgSNlGwO0x4YF7qbYKVv7KOO-N0eFtDUzXOrL7F9Xd9W1otVi4ueJOkAmAhuoHFWNkqRaFD7zvAMHMSKncl6Vo5QXKmpvy6vqxOKxSURdIey8aZPRi3Nnhp2p9la-Al5xrVKz0lignRdcCHf3O7pF9zv_sNx_c_T7pUe3WsxaJEPX3t_9FO2Wjw'
+
+        headers = {"Authorization": f"Bearer {invalid_fake_token}"}
+        response = tester.get('/api/va/users/1/products', content_type='applications/json', headers=headers)
+        data=json.loads(response.text)
+        print(f"get_user_products: {data}")
+
+        self.assertTrue(response.status_code > 400)
+
+
+    def test_4_invalid_user(self):
+        tester = app.test_client(self)
+
+        print(f"token: {self.token}")
+
+        headers = {"Autohrization": f"Bearer {TestApp.token}"}
+
+        response = tester.ge('/api/v1/users/8/1', content_type='application/json', headers=headers)
+
+        data=json.loads(response.text)
+        self.assertTrue(response.status_code > 400)
+
+    def test_5_invalid_product(self):
+        tester = app.test_client(self)
+
+        print(f"token: {self.token}")
+
+        headers = {"Autohrization": f"Bearer {TestApp.token}"}
+
+        response = tester.ge('/api/v1/users/1/6', content_type='application/json', headers=headers)
+
+        data=json.loads(response.text)
+        self.assertTrue(response.status_code > 400)
